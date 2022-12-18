0ba8f7ae8caf53d84c05c164324ca4de63f01fe6
Sally az <sallyazulay@gmail.com>
2022-12-18 14:48:34 +0200

    install and setup apollo client

--- App.tsx
+++ App.tsx
@@ -5,6 +5,20 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+
+const API_KEY = "mounteliza::stepzen.net+1000::46da93dc5972b3c5f265ba6b206c4d481c41e0a25bee9ee06a4582780a955968";
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: "https://mounteliza.stepzen.net/api/newbie-quoll/__graphql",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
@@ -14,8 +28,10 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </ApolloProvider>
       </SafeAreaProvider>
     );
   }
