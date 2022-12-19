96e2e8d9958d0b888c05773f6cbdd0155a703593
Sally az <sallyazulay@gmail.com>
2022-12-19 18:07:48 +0200

    added my book screen component

---
+++ screens/MyBooksScreen/MyBooksScreen.tsx
@@ -0,0 +1,34 @@
+import { FlatList, StyleSheet } from "react-native";
+import BookItem from "../../components/BookItem";
+import { View } from "../../components/Themed";
+
+import { useBooks } from "../../context/MyBooksProvider";
+
+export default function MyBooksScreen() {
+  const { savedBooks } = useBooks();
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={savedBooks}
+        renderItem={({ item }) => <BookItem book={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
